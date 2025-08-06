fe2322b9fbdfcbe34d8db09c4b45ffaa6b778477
gaalr <gaalruben02@gmail.com>
2025-08-06 23:31:38 +0200

    init

---
+++ test_files/file1.py
@@ -0,0 +1,6 @@
+def add(a, b):
+    "this should overlap"
+    return a + b
+
+def multiply(a, b):
+    return a * b
---
+++ test_files/file2.py
@@ -0,0 +1,7 @@
+def add2(a,b):
+    "this should overlap"
+    return a + b
+
+def multiply(a, b):
+    "this should overlap"
+    return a * b
